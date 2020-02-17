8b5bd2f51737a60e90250400c5af40b2159ba31c
Natalia Takahara <nana_takahara@hotmail.com>
2020-02-17 17:31:35 +0900

    parse part

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,19 @@
+require 'json'
+require 'open-uri'
+
+class GamesController < ApplicationController
+  def new
+    @letters = ('a'..'z').to_a.sample(10)
+  end
+
+  def score
+    @word = params[:word]
+    @letters = params[:letters].split
+    @word.chars.all? { |letter| @word.count(letter) <= @letters.count(letter) }
+
+    url = 'https://wagon-dictionary.herokuapp.com/'
+    en_word = open(url).read
+     result = JSON.parse(en_word)
+  end
+
+end
